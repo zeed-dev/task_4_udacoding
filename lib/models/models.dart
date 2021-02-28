1b25dc5c5f13fd51da248b4ecde581aabb829683
MUHAMMAD ZIAD ALFIAN <muhammadziadalfian07@gmail.com>
2021-02-28 14:07:54 +0800

    Final

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -3,5 +3,5 @@ import 'package:equatable/equatable.dart';
 part 'farm.dart';
 part 'news.dart';
 part 'dictionary.dart';
-part 'api_return_vaue.dart';
+part 'api_return_value.dart';
 part 'user.dart';
